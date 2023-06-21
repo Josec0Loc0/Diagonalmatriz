f02b6cf4f9f227a694bacbefedee9099c4edd743
Josec0Loc0 <130604593+Josec0Loc0@users.noreply.github.com>
2023-06-21 17:02:04 -0500

    Add files via upload

---
+++ Diagonalrecoleccion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int f, c;
+    printf("Ingrese el numero de filas que desea en la matriz: ");
+    scanf("%d", &f);
+   
+    printf("Ingrese el numero de columnas que desea en la matriz: ");
+    scanf("%d", &c);
+   
+    return 0;
+}
